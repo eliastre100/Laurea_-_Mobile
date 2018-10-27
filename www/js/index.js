a2dcd82a431f66e21c29fc3895051f36f4b85008
Antoine FORET <antoine.foret@epitech.eu>
2018-10-27 23:56:37 +0300

    [+] Added Login page

---
+++ www/js/index.js
@@ -0,0 +1,13 @@
+include('page','login');
+
+function login() {
+    loadingElement('loginBtn', 'Please wait...');
+    setTimeout(function () {
+        if ($('#username').val() === 'admin' && $('#password').val() == 'azerty') {
+            openPage('home');
+        } else {
+            alert('Bad username or password. Hint: admin && french keyboard');
+        }
+        closeLoading('loginBtn');
+    }, 1000);
+}
