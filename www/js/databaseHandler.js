d21e309a6ba80ff77b1c01daedc1de03536a2367
Antoine FORET <antoine.foret@epitech.eu>
2018-11-01 14:53:56 +0200

    [+] Added database creation

---
+++ www/js/databaseHandler.js
@@ -0,0 +1,21 @@
+let databaseHandler = {
+    db: null,
+    createDatabase:  () => {
+        this.db = window.openDatabase(
+            "UserManager.db",
+            "1.0",
+            "accounts database",
+            1000000);
+        this.db.transaction((tx) => {
+                tx.executeSql(
+                    "create table if not exists accounts(_id int primary key, username text, password text)",
+                    [],
+                    (tx, results) => { },
+                    (tx, error) => { console.log("Error while creating the table: " + error.message); }
+                );
+            },
+            (error) =>{ console.log("Transaction error:" + error.message); },
+            () => { console.log("Create DB transaction completed successfully:" ); },
+        );
+    }
+};
---
+++ www/js/index.js
@@ -0,0 +1,3 @@
+$(document).on("ready", () => {
+   databaseHandler.createDatabase()
+});
