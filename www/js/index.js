9f0ca4a67f00ccb9222017ac779f5bd487942278
Antoine FORET <antoine.foret@epitech.eu>
2018-11-02 13:59:59 +0200

    [+] Added add page

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,24 @@
 $(document).on("ready", () => {
    databaseHandler.createDatabase()
 });
+
+function addAccount() {
+    const id = $("#id").val();
+    const username = $("#username").val();
+    const password = $("#password").val();
+    const confirmPassword = $("#passwordConfirm").val();
+
+    if (id !== "" && username !== "" && password !== "" && password === confirmPassword) {
+        accounttHandler.addAccount(id, username, password).then(() => {
+            $("#id").val("");
+            $("#username").val("");
+            $('#password').val("");
+            $('#passwordConfirm').val("");
+            alert('Account registered');
+        }) .catch(() => {
+            alert('Unable to save account');
+        })
+    } else {
+        alert("Invalid parameters");
+    }
+}
